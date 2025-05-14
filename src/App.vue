40c5956915fab57e8b0dc8b52c7e381435fc7f5f
Shu Matsumoto <shumatsumoto0328@gmail.com>
2025-05-14 13:37:21 +0900

    todo02

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,9 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
