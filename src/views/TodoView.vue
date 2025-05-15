36d36f79407b870868ef521e5c428aa5c0299f12
Shu Matsumoto <shumatsumoto0328@gmail.com>
2025-05-15 11:53:22 +0900

    todo06

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -6,6 +6,19 @@ const todos = ref([
   { id: 2, title: "タイトル02", done: false },
   { id: 3, title: "タイトル03", done: false },
 ]);
+
+const newTodo = ref("");
+
+const addTodo = () => {
+  console.log(newTodo.value);
+  const todo = {
+    id: todos.value.length + 1,
+    title: newTodo.value,
+    done: false,
+  };
+  todos.value.push(todo);
+  newTodo.value = "";
+};
 </script>
 
 <template>
@@ -15,16 +28,17 @@ const todos = ref([
     <div class="px-4 py-2">
       <h1 class="text-gray-800 font-bold text-2xl uppercase">To-Do List</h1>
     </div>
-    <form class="w-full max-w-sm mx-auto px-4 py-2">
+    <form class="w-full max-w-sm mx-auto px-4 py-2" @submit.prevent="addTodo">
       <div class="flex items-center border-b-2 border-teal-500 py-2">
         <input
           class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
           type="text"
           placeholder="Add a task"
+          v-model="newTodo"
         />
         <button
           class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
-          type="button"
+          type="submit"
         >
           Add
         </button>
